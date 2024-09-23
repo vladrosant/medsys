70604f028d653cc2e8fde5ef8b85801dff7c9e57
vladrosant <pedromuch1@gmail.com>
2024-09-22 21:48:44 -0300

    Add REST API for managing patients

---
+++ patients/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import PatientViewSet
+
+router = DefaultRouter()
+router.register(r'patients', PatientViewSet)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include(patients.urls)),
+]
